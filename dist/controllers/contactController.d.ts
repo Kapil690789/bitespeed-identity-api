7928f9e3c2296f7a72bc0e5da5a6771e07824014
Kapilflockai <kapil@flockshop.in>
2025-07-16 19:03:05 +0530

    added_Changes_prisma

--- dist/controllers/contactController.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-export declare class ContactController {
-    private contactService;
-    constructor();
-    identify: (req: Request, res: Response) => Promise<void>;
-    healthCheck: (req: Request, res: Response) => Promise<void>;
-}
-//# sourceMappingURL=contactController.d.ts.map
